43b6f2a0680e0e736a0e78598e2c72e94e3cd5c6
Willy <zhamatsistemas@gmail.com>
2020-11-01 12:48:34 -0500

    envia correo

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,7 +1,47 @@
+const nodemailer = require('nodemailer')
+
 const indexCtrl = {};
 
 indexCtrl.indexPrincipal = (req, res) => {
   res.render('index');  
 };
+
+indexCtrl.enviaEmail = async (req, res) => {
+  
+  const {nombre, email, whatsapp, mensaje} = req.body
+  contentHTML = `
+    <h3>Solicitud de Información para CréditoSonivision</h3>
+    <ul><h4>
+      <li>Cliente: ${nombre}</li>
+      <li>Email de Uusario: ${email}</li>
+      <li>Teléfono: ${whatsapp}</li>    
+    </h4></ul>
+    <p><h4>Mensaje: ${mensaje}</h4></p>`
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAIL_SERVICE,
+      port: process.env.PUERTO,
+      secure: true,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+      }
+    })
+    
+    const mailOptions = {
+      from: process.env.MAIL_FROM, 
+      to: process.env.MAIL_TO,
+      subject: 'Informacion a Zhamat Sistemas',
+      html: contentHTML
+    }
+
+    await transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+          return console.log(error);
+      }
+      console.log('mensaje enviado', info.messageId, info.response)
+      res.render('emailok');
+    });  
+}
 
 module.exports = indexCtrl;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 
 const {
-  indexPrincipal
+  indexPrincipal,
+  enviaEmail
 } = require("../controllers/index.controller");
 
 // Routes
 router.get("/", indexPrincipal);
+
+router.post('/enviarEmail', enviaEmail)
 
 module.exports = router;
