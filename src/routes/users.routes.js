40b6c6df856bf6fba6d501d8b8dd0ba682b9bc3d
Willy <zhamatsistemas@gmail.com>
2020-11-01 21:14:41 -0500

    variables de entorno

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,15 +3,21 @@ const router = require("express").Router();
 const {
   iniciarSesion,
   registrarse,
-  singup
+  singup,
+  signin,
+  logout
 } = require("../controllers/users.controller");
 
 // Routes
 router.get("/users/signin", iniciarSesion);
 
+router.post("/users/signin", signin);
+
 router.get("/users/signup", registrarse);
 
 router.post("/users/signup", singup)
+
+router.get("/users/logout", logout);
 
 module.exports = router;
 
